2b9d8936e767802ead3e17c3e0f3a8c6cc938850
UltimateForm <heliojmsemedo@gmail.com>
2021-12-05 13:58:49 +0000

    Responsiveness (#4)
    * added code for library page
    
    * working library
    
    * added responsiveness to header and general font size
    
    * responsive poem view

---
+++ src/components/Divider.tsx
@@ -0,0 +1,5 @@
+export function Divider() {
+	return (
+		<div className="bg-riverBed transition-colors ease-in-out duration-200 divider h-3/5 w-px absolute left-1/2 place-self-center hidden lg:block" />
+	);
+}
--- src/layout/AppView/index.tsx
+++ src/layout/AppView/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export function Fullscreen(props: { children: React.ReactNode }) {
 	const { children } = props;
 	return (
-		<div className="h-screen flex items-center p-5">
+		<div className="h-screen flex items-center p-5 text-sm lg:text-xl xl:text-2xl pt-16">
 			{children}
 			<Header />
 		</div>
